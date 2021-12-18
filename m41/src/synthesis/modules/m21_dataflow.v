6e6f94a82e6c836cf2fd7989d7f26f115344e099
mdodovic <matija.dodovic@gmail.com>
2021-12-18 12:41:57 +0100

    create multiplexer 4x1

--- m41/src/synthesis/modules/m21_dataflow.v
+++ m41/src/synthesis/modules/m21_dataflow.v
@@ -1,8 +1,19 @@
-module m21_dataflow(I0, I1, S0, Y);
+module m41(
+	input I0, 
+	input I1, 
+	input I2, 
+	input I3, 
+	input [1:0] S, 
+	output reg Y
+);
 
-	input I0, I1, S0;
-	output Y;
-
-	assign Y = S0 ? I1 : I0;
+always @(I0, I1, I2, I3, S) begin
+	case (S)
+		2'b00: Y = I0; 
+		2'b01: Y = I1; 
+		2'b10: Y = I2; 
+		2'b11: Y = I3; 
+	endcase
+end
 
 endmodule 
