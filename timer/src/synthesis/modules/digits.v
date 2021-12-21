4e5bb7a73aef5b65025d55fe0dc5b4c6b521b3d0
mdodovic <matija.dodovic@gmail.com>
2021-12-21 14:02:26 +0100

    add motivation for multiple generation

--- timer/src/synthesis/modules/digits.v
+++ timer/src/synthesis/modules/digits.v
@@ -5,12 +5,16 @@ module digits (
     output [6:0] out_hundreds,
     output [6:0] out_thousands
 );
-    
+    // multiple and conditionally instancing
+    // generate: for in harwdare to instance multiple times
+
+    // multiple instancing:
     wire [3:0] ones      = in / 1    % 10;
     wire [3:0] tens      = in / 10   % 10;
     wire [3:0] hundreds  = in / 100  % 10;
     wire [3:0] thousands = in / 1000 % 10;
 
+    // multiple instancig:
     hex hex_inst1(ones, out_ones);
     hex hex_inst10(tens, out_tens);
     hex hex_inst100(hundreds, out_hundreds);
