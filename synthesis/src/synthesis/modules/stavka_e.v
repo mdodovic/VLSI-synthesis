759618d0d1e0e638be556d4b319284d91010714c
mdodovic <matija.dodovic@gmail.com>
2021-12-22 12:01:27 +0100

    test synthesis of stavka_e

--- synthesis/src/synthesis/modules/stavka_e.v
+++ synthesis/src/synthesis/modules/stavka_e.v
@@ -1,4 +1,6 @@
-module stavka_d (
+module stavka_e #(
+    parameter NUM = 50_000_000
+)(
     input rst_n,
     input clk,
     input select,
@@ -15,11 +17,12 @@ module stavka_d (
     stavka_a red_next(clk, rst_n, next, next_red);
 
     // we have states:
-    localparam setup = 1'b0;
-    localparam gcd = 1'b1;
+    localparam setup = 2'b00;
+    localparam gcd = 2'b01;
+    localparam decrement = 2'b10;
 
     // and our current state:
-    reg current_state_reg, current_state_next;
+    reg[1:0] current_state_reg, current_state_next;
     
     reg [3:0] buffer_reg [1:0]; 
     reg [3:0] buffer_next [1:0]; 
@@ -36,6 +39,9 @@ module stavka_d (
     reg [3:0] result_next [1:0]; 
 
 
+    integer counter_next, counter_reg;
+
+
     always @(posedge clk, negedge rst_n) begin
         if(!rst_n) begin
             for (i = 0; i < 2; i = i + 1) begin
@@ -45,6 +51,7 @@ module stavka_d (
             data_out_reg <= 4'h0;
             current_state_reg <= setup;
             gcd_reg <= 4'h0;
+            counter_reg <= 0;;
 
         end else begin
             for (i = 0; i < 2; i = i + 1) begin
@@ -54,12 +61,12 @@ module stavka_d (
             data_out_reg <= data_out_next;
             current_state_reg <= current_state_next;
             gcd_reg <= gcd_next;
+            counter_reg <= counter_next;
         end
     end
 
 
     always @(*) begin
-        // we will always set data_out_next to buffer[select], so we do not need initial initialization
         data_out_next = data_out_reg;
         current_state_next = current_state_reg;
         gcd_next = gcd_reg;
@@ -68,6 +75,7 @@ module stavka_d (
             result_next[i] = result_reg[i];
         end
         j = 4'd0;
+        counter_next = counter_reg;
 
         case (current_state_reg)
             setup: begin
@@ -81,9 +89,7 @@ module stavka_d (
             gcd: begin
                 if(next_red) begin
                     // again rising edge - change state
-                    current_state_next = setup;
-                    // this is good, because we need output for changed state - here it is easy because output for setup state is just buffer[select]
-                    data_out_next = buffer_next[select];
+                    current_state_next = decrement;                    
                 end
 
                 for (j = 4'd1; j < 4'd10 ; j = j + 1'd1) begin
@@ -92,6 +98,26 @@ module stavka_d (
                 end
 
                 data_out_next = buffer_next[select] / gcd_next;
+
+            end 
+            decrement: begin
+                if(next_red) begin
+                    if((buffer_reg[0] % gcd_reg == 4'd0) && (buffer_reg[1] % gcd_reg == 4'd0)) begin
+                        current_state_next = setup;
+                        data_out_next = buffer_next[select];
+                    end
+                end
+                
+                if(counter_reg == NUM) begin
+                    counter_next = 0; 
+                    gcd_next = gcd_reg - 4'd1;
+                    if(gcd_next == 4'd0) begin
+                        current_state_next = setup;
+                        data_out_next = buffer_next[select];
+                    end
+                end else begin
+                    counter_next = counter_reg + 1;
+                end
 
             end 
         endcase
